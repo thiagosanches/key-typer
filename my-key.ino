d0cbd3f072df23cda65577bf9ca3cb4601c8f78b
Thiago Sanches <tsigwt@gmail.com>
2021-08-14 10:26:01 -0300

    Update my-key.ino

--- my-key.ino
+++ my-key.ino
@@ -1,17 +1,37 @@
 #include <Keyboard.h>
-bool done = false;
+
+const int pinButton1 = 2;
+const int pinButton2 = 9;
+
+int button1 = 0;
+int button2 = 0;
+
 void setup() {
+  pinMode(pinButton1, INPUT);
+  pinMode(pinButton2, INPUT);
   Keyboard.begin();
   delay(500);
 }
 
 void loop() {
-  delay(500);
-  if (!done) {
-    done = true;
-    Keyboard.print("MY-LONG-PASSWORD-GOES-HERE");
+  button1 = digitalRead(pinButton1);
+  button2 = digitalRead(pinButton2);
+
+  if(button1 == HIGH){
+    Serial.println("BUTTON1");
+    
+    Keyboard.print("YOUR TEXT GOES HERE");
     Keyboard.press(KEY_RETURN);
-    //THIS IS SO IMPORTANT, IF YOU FORGET IT, YOU ARE BRICKED.
     Keyboard.releaseAll();
   }
+
+  if(button2 == HIGH){
+    Serial.println("BUTTON2");
+
+    Keyboard.print("YOUR TEXT GOES HERE");
+    Keyboard.press(KEY_RETURN);
+    Keyboard.releaseAll();
+  }
+
+  delay(250);
 }
